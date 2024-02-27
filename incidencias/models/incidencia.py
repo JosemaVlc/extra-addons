108d6930c7e9819d8e34459ead198b2c4bca0341
JosemaVlc <j.moreno.bolivar@gmail.com>
2024-02-27 17:17:33 +0000

    quito varios computados por related

--- incidencias/models/incidencia.py
+++ incidencias/models/incidencia.py
@@ -16,23 +16,24 @@ class incidencia(models.Model):
     fase = fields.Selection([('0', 'Entrada'), ('1', 'En Progreso'), ('2', 'Solucionada')], required=True, default='0')
     incidence_text = fields.Text(string = 'Texto de la incidencia', required=True)
     solution_text = fields.Text(string = 'Texto de la solución')
-    service_tlf = fields.Boolean(string = 'Tlf', compute='_compute_client_info')
-    service_tv = fields.Boolean(string = 'Tv', compute='_compute_client_info')
-    service_net = fields.Boolean(string = 'Net', compute='_compute_client_info')
-    cliente_nif = fields.Char(string='NIF', compute='_compute_client_info')
-    cliente_nombre = fields.Char(string='Nombre', compute='_compute_client_info')
-    cliente_calle = fields.Char(string='Direccion', compute='_compute_client_info', store=True)
-    cliente_ciudad = fields.Char(string='Municipio', compute='_compute_client_info', store=True)
+    service_tlf = fields.Boolean(string = 'Tlf', related="contrato_id.service_tlf")
+    service_tv = fields.Boolean(string = 'Tv', related="contrato_id.service_tv")
+    service_net = fields.Boolean(string = 'Net', related="contrato_id.service_net")
+    cliente_nif = fields.Char(string='NIF', related="contrato_id.partner_id.vat", store=True)
+    cliente_nombre = fields.Char(string='Nombre', related='contrato_id.partner_id.name')
+    cliente_calle = fields.Char(string='Direccion', related='contrato_id.partner_id.street', store=True)
+    cliente_ciudad = fields.Char(string='Municipio', related='contrato_id.partner_id.city', store=True)
+    cliente_telefono = fields.Char(string='Telefono', related='contrato_id.partner_id.phone')
+    cliente_movil = fields.Char(string='Movil', related='contrato_id.partner_id.mobile')
+    cliente_email = fields.Char(string='Email', related='contrato_id.partner_id.email')
+    cliente_zona_tecnica = fields.Char(string='Zona Tecnica', related='contrato_id.zona_id.name')
+
     cliente_provincia = fields.Char(string='Provincia', compute='_compute_client_info', store=True)
-    cliente_telefono = fields.Char(string='Telefono', compute='_compute_client_info')
-    cliente_movil = fields.Char(string='Movil', compute='_compute_client_info')
-    cliente_email = fields.Char(string='Email', compute='_compute_client_info')
-    cliente_zona_tecnica = fields.Char(string='Zona Tecnica', compute='_compute_client_info')
     cliente_tecnico_asociado = fields.Char(string="Tecnico Asociado", compute='_compute_client_info')
 
     
     # Relacion contrato [1:N] incidencia
-    contrato_id = fields.Many2one('incidencias.contrato', readonly="True") # devolverá 1 contrato.
+    contrato_id = fields.Many2one('incidencias.contrato', readonly="True", store=True) # devolverá 1 contrato.
     # Relacion anotacion [N:1] incidencia
     annotations_ids = fields.One2many('incidencias.anotacion', 'incidencia_id') # devolverá todas las anotaciones.
     
@@ -50,35 +51,12 @@ class incidencia(models.Model):
     def _compute_client_info(self):
         for incidencia in self:
             if incidencia.contrato_id:
-                self.service_net = incidencia.contrato_id.service_net
-                self.service_tlf = incidencia.contrato_id.service_tlf
-                self.service_tv = incidencia.contrato_id.service_tv
-                self.cliente_nif = incidencia.contrato_id.partner_id.vat
-                self.cliente_nombre = incidencia.contrato_id.partner_id.name
-                self.cliente_calle = incidencia.contrato_id.partner_id.street
-                self.cliente_ciudad = incidencia.contrato_id.partner_id.city
                 state_id = incidencia.contrato_id.partner_id.state_id.id
                 state_record = incidencia.env['res.country.state'].browse(state_id)
-                self.cliente_telefono = incidencia.contrato_id.partner_id.phone
-                self.cliente_movil = incidencia.contrato_id.partner_id.mobile
-                self.cliente_email = incidencia.contrato_id.partner_id.email
                 self.cliente_provincia = state_record.name
-                self.cliente_zona_tecnica = incidencia.contrato_id.zona_id.name
                 self.cliente_tecnico_asociado = incidencia.contrato_id.zona_id.tecnicos_ids.name
             else:
-                self.cliente_nif = ""
-                self.cliente_nombre = ""
-                self.cliente_calle = ""
-                self.cliente_ciudad = ""
                 self.cliente_provincia = ""
-                self.cliente_telefono = ""
-                self.cliente_movil = ""
-                self.cliente_email = ""
-                self.service_net = ""
-                self.service_tlf = ""
-                self.service_tv = ""
-                self.cliente_nif = ""
-                self.cliente_zona_tecnica = ""
                 self.cliente_tecnico_asociado = ""
 
     def funcion_confirmar(self):
@@ -96,3 +74,7 @@ class incidencia(models.Model):
                 futuro_estado = int(self.fase)+1
                 self.write({'fase':str(futuro_estado)})
                 self.fecha_solucion = fields.Datetime.now()
+
+    def funcion_cambiar_tecnico(self):
+        pass
+
