5433b2e884674cfbb497f6a7f30e44822e3bbb52
JosemaVlc <j.moreno.bolivar@gmail.com>
2024-03-01 16:54:08 +0000

    albaran sumatorio

--- incidencias/models/albaran.py
+++ incidencias/models/albaran.py
@@ -7,15 +7,16 @@ class incidencia(models.Model):
     _name = 'incidencias.albaran'
     _description = 'Albaran'
     
-    name= fields.Char(string="Nº Albaran", readonly=1)
+    name= fields.Char(string="Nº Albaran", readonly=1, string="Incidencia")
 
-    incidencia_id = fields.Many2one('incidencias.incidencia', compute='compute_albaran', inverse='stage_inverse')
+    incidencia_id = fields.Many2one('incidencias.incidencia', compute='_compute_albaran', inverse='stage_inverse')
     # Relacion lineas [N:1] albaran
     lineas_ids = fields.One2many('incidencias.linea', 'albaran_id') #Devolverá todas las lineas.
     incidencias_ids = fields.One2many('incidencias.incidencia', 'albaran_id', invisible=True, readonly=True)
+    total_price = fields.Float(compute='_compute_total', readonly=True, string="Total") 
 
     @api.depends('incidencias_ids')
-    def compute_albaran(self):
+    def _compute_albaran(self):
         if len(self.incidencias_ids) > 0:
             self.incidencia_id = self.incidencias_ids[0]
 
@@ -35,3 +36,10 @@ class incidencia(models.Model):
     def create(self, vals):
         vals['name'] = self.env['ir.sequence'].next_by_code('albaranes_secuencia')
         return super(incidencia, self).create(vals)
+    
+    @api.depends('lineas_ids.price')
+    def _compute_total(self):
+        for record in self:
+            total = sum(linea.price for linea in record.lineas_ids)
+            record.total_price = total
+
--- incidencias/models/linea.py
+++ incidencias/models/linea.py
@@ -8,9 +8,19 @@ class incidencia(models.Model):
     _description = 'Linea'
 
     # Relacion material [1:N] linea
-    material_id = fields.Many2one('incidencias.material',string='Producto') # Devolverá el material asociado.
-
+    material_id = fields.Many2one('incidencias.material', string='Producto') # Devolverá el material asociado.
+    code = fields.Char(related="material_id.code", string="Codigo")
+    unit_price = fields.Float(related="material_id.price", string="Precio Unitario")
     quantity = fields.Integer(string='Cantidad')
+    price = fields.Float(compute="_compute_price")
 
     # Relacion albaran [1:N] linea
     albaran_id = fields.Many2one('incidencias.albaran', readonly=True) # Devolverá el albarán asociado.
+
+    @api.depends('material_id', 'quantity', 'quantity')
+    def _compute_price(self):
+        for record in self:
+            if (record.material_id and record.unit_price and record.quantity):
+                self.price = self.unit_price * self.quantity
+            else:
+                self.price = 0.0
